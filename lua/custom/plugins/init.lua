c845fc4d0e8eed96edc7425e4394f8bf3c8aa8f1
Danis <danis.ramirez.hn@gmail.com>
2024-06-02 09:47:55 +0200

    add flutter-tools

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,11 @@
 -- See the danny.nvim README for more information
 
-return {}
+return {
+	"akinsho/flutter-tools.nvim",
+	lazy = false,
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"stevearc/dressing.nvim", -- optional for vim.ui.select
+	},
+	config = true,
+}
