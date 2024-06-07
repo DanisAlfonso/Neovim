079904fc855b8834322cfbf2af350076ae2337a3
Danis <danis.ramirez.hn@gmail.com>
2024-06-07 14:55:07 +0200

    reorder plugins/init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,18 +1,19 @@
 -- See the danny.nvim README for more information
 
 return {
-	"akinsho/flutter-tools.nvim",
-	lazy = false,
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"stevearc/dressing.nvim", -- optional for vim.ui.select
+	{ "mfussenegger/nvim-dap" },
+	{
+		"akinsho/flutter-tools.nvim",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim", -- optional for vim.ui.select
+		},
+		config = true,
 	},
-	config = true,
-	"Civitasv/cmake-tools.nvim",
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	"xiyaowong/transparent.nvim",
-	"olimorris/onedarkpro.nvim",
+	{ "Civitasv/cmake-tools.nvim" },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "xiyaowong/transparent.nvim" },
+	{ "olimorris/onedarkpro.nvim" },
 	priority = 1000,
 }
