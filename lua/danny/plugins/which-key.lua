023abdd8b3ee0f656844064455a51835164b4c57
Danis <danis.ramirez.hn@gmail.com>
2024-07-24 21:12:23 +0200

    bug fix in which-key

--- lua/danny/plugins/which-key.lua
+++ lua/danny/plugins/which-key.lua
@@ -1,22 +1,61 @@
 return {
 	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
+	event = "VeryLazy",
+	opts = {
+		-- Your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		plugins = {
+			marks = true, -- shows a list of your marks on ' and `
+			registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+			spelling = {
+				enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+				suggestions = 20, -- how many suggestions should be shown in the list?
+			},
+			presets = {
+				operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+				motions = true, -- adds help for motions
+				text_objects = true, -- help for text objects triggered after entering an operator
+				windows = true, -- default bindings on <c-w>
+				nav = true, -- misc bindings to work with windows
+				z = true, -- bindings for folds, spelling and others prefixed with z
+				g = true, -- bindings for prefixed with g
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
+	config = function()
 		require("which-key").setup()
 
-		-- Document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-			["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+		-- Document existing key chains with the new format
+		require("which-key").add({
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>c_", hidden = true },
+			{ "<leader>d", group = "[D]ocument" },
+			{ "<leader>d_", hidden = true },
+			{ "<leader>r", group = "[R]ename" },
+			{ "<leader>r_", hidden = true },
+			{ "<leader>s", group = "[S]earch" },
+			{ "<leader>s_", hidden = true },
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>w_", hidden = true },
+			{ "<leader>t", group = "[T]oggle" },
+			{ "<leader>t_", hidden = true },
+			{ "<leader>h", group = "Git [H]unk" },
+			{ "<leader>h_", hidden = true },
 		})
-		-- visual mode
-		require("which-key").register({
-			["<leader>h"] = { "Git [H]unk" },
-		}, { mode = "v" })
+
+		-- Visual mode
+		require("which-key").add({
+			{ "<leader>h", desc = "Git [H]unk", mode = "v" },
+		})
 	end,
 }
