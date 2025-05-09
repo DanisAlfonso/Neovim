a465432f94ba5af386f81b2b2803632e9a627eb8
Danis <danis.ramirez.hn@gmail.com>
2025-05-10 01:08:53 +0200

    Remove unused nvim-training plugin

--- lua/danny/plugins/nvim-training.lua
+++
@@ -1,6 +0,0 @@
--- Plugin for nvim-training
-return {
-	"Weyaaron/nvim-training",
-	opts = {},
-	-- cmd = { "TrainingStart", "TrainingStop", "TrainingToggle", "TrainingStats" }, -- Removed cmd for eager loading
-}
